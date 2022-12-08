9d54bfaeaec5d0e66c68c6c54f2adae12ab279ea
PrestonFr <PrestonFr@gmail.com>
2022-12-08 17:06:21 -0500

    added recipeIngredient controllers

---
+++ controllers/recipeIngredients_controller.js
@@ -0,0 +1,84 @@
+const recipeIngredients = require('express').Router()
+const db = require('../models')
+const { Recipe, Ingredient, RecipeIngredient } = db 
+
+// FIND ALL INGREDIENTS
+recipeIngredients.get('/', async (req, res) => {
+    try{
+        const foundRecipeIngredients = await RecipeIngredient.findAll()
+        res.status(200).json(foundRecipeIngredients)
+        console.log("success")
+    } catch (error) {
+        res.status(500).json(error)
+        console.log("broken")
+    }
+})
+
+//FIND A SPECIFIC INGREDIENT
+recipeIngredients.get('/:id', async (req, res) => {
+    try {
+        const foundRecipeIngredient = await RecipeIngredient.findOne({
+            where: { recipe_ingredient_id: req.params.id }
+        })
+        res.status(200).json(foundRecipeIngredient)
+        console.log("Nice Work")
+    } catch (error) {
+        res.status(500).json(error)
+        console.log(error)
+    }
+})
+
+// CREATE A NEW INGREDIENT
+recipeIngredients.post('/', async (req, res) => {
+    try {
+        const newRecipeIngredient = await RecipeIngredient.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new recipe ingredient',
+            data: newRecipeIngredient
+        });
+        console.log("GREEEENNNN")
+    } catch(err) {
+        console.log("broke")
+        res.status(500).json(err)
+    }
+});
+
+
+// UPDATE AN INGREDIENT
+recipeIngredients.put('/:id', async (req, res) => {
+    try {
+        const updatedRecipeIngredient = await RecipeIngredient.update(req.body, {
+            where: {
+                recipe_ingredient_id: req.params.id
+            }
+        });
+        res.status(200).json({
+            message: `Successfully updated`,
+            data: updatedRecipeIngredient
+        });
+    } catch(err) {
+        res.status(500).json(err)
+        console.log("broke")
+    }
+});
+
+
+// DELETE A RECIPE
+recipeIngredients.delete('/:id', async (req, res) => {
+    try {
+        const deletedRecipeIngredient = await RecipeIngredient.destroy({
+            where: {
+                recipe_ingredient_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted`,
+            data: deletedRecipeIngredient
+        })
+    } catch(err) {
+        res.status(500).json(err)
+        console.log("broken")
+    }
+})
+
+module.exports = recipeIngredients
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const PORT = process.env.PORT || 3010;
 const express = require("express");
 const app = express();
 const { Sequelize } = require('sequelize')
-const recipes = require('./controllers/recipes_controller')
 
 // CONFIGURATION / MIDDLEWARE
 app.use(express.json())
@@ -30,63 +29,10 @@ app.use('/api/recipes', recipesController)
 const ingredientsController  = require('./controllers/ingredients_controller')
 app.use('/api/ingredients', ingredientsController)
 
-// const stagesController = require('./controllers/stages_controller')
-// app.use('/stages', stagesController)
+const recipeIngredientsController = require('./controllers/recipeIngredients_controller')
+app.use('/api/recipeIngredients', recipeIngredientsController)
 
-// app.use(express.json());
-
-// //retrieving recipes http://localhost:3005/getRecipes
-// app.get("/api/recipes", (req, res) => {
-//     console.log("route handler ran");
-//     res.status(200).json({
-//         status: "success",
-//         data: {
-//             countries: ["america", "canada", "russia"],
-//         },
-//     });
-// });
-
-// //Retrieve one recipe
-// app.get("/api/recipes/:id", (req, res) => {
-//     console.log(req);
-//     res.status(200).json({
-//         status: "success",
-//         data: {
-//             countries: ["america", "canada", "russia"],
-//         },
-//     });
-// });
-
-// //Creating new recipe
-// app.post("/api/recipes", (req, res) => {
-//     console.log(req.body);
-//     res.status(201).json({
-//         status: "success",
-//         data: {
-//             countries: ["america", "canada", "russia"],
-//         },
-//     });
-// });
-
-// //Update recipes
-// app.put("/api/recipes/:id", (req, res) => {
-//     console.log(req.params.id);
-//     console.log(req.body);
-//     res.status(200).json({
-//         status: "success",
-//         data: {
-//             countries: ["america", "canada", "russia"],
-//         },
-//     });
-// });
-
-// //Delete recipe
-// app.delete("/api/recipes/:id", (req, res) => {
-//     res.status(204).json({
-//         status: "success",
-//     });
-// });
 
 app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
+    console.log(`💪 Signal on port: ${process.env.PORT}`);
 });
